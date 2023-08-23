11b7ec076c878d7eef1da189abdc2476cdb4383b
jordanvidal1 <jv@coinburp.com>
2023-08-23 17:07:24 +0100

    feat: connect resources page

--- src/view/shared/Resources.jsx
+++ src/view/shared/Resources.jsx
@@ -3,12 +3,13 @@ import {Link} from 'react-router-dom';
 import {Grid} from '@material-ui/core';
 import {isHuddle} from '../../services/helper';
 import Resource from './Resource';
+import Loader from './Loader';
 
 import ArrowIcon from '../../static/huddle/arrow.svg';
 import UnitasArrowIcon from '../../static/unitas/request-arrow.svg';
 
 const Resources = props => {
-  const {resources, name} = props;
+  const {name, resources, isLoading} = props;
 
   return (
     <div className='box resources-box'>
@@ -21,11 +22,14 @@ const Resources = props => {
           <img alt='arrow' src={isHuddle ? ArrowIcon : UnitasArrowIcon} />
         </Link>
       </div>
-      <Grid container spacing={3} className='resources-container'>
-        {resources?.length > 0 && resources.slice(0, 12).map((resource, i) => (
-          <Resource i={i} {...resource} />
-        ))}
-      </Grid>
+      {isLoading && resources.length < 1 ? <Loader />
+        : (
+          <Grid container spacing={3} className='resources-container'>
+            {resources?.length > 0 && resources.slice(0, 12).map((resource, i) => (
+              <Resource i={i} {...resource} />
+            ))}
+          </Grid>
+        )}
     </div>
   );
 }
--- src/view/shared/ResourcesPage.jsx
+++ src/view/shared/ResourcesPage.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Resources from './Resources';
+import useBlog from '../../services/hooks/useBlog';
 
 const ResourcesPage = props => {
   const {name} = props;
+
+  const {
+    loadBlog,
+    isBlogLoading,
+    blogPosts,
+  } = useBlog();
+
+  useEffect(() => {
+    loadBlog();
+  }, []);
 
   return (
     <div className='related-resources'>
       <div className='container'>
         <div className='inner-container'>
           <div className='content'>
-            <Resources name={name} />
+            <Resources
+              name={name}
+              resources={blogPosts}
+              isLoading={isBlogLoading}
+            />
           </div>
         </div>
       </div>
