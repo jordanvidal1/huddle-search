29eeaa0b5bf741b8c8f79dced5e6b397fad00df0
jordanvidal1 <jv@coinburp.com>
2023-09-01 13:43:12 +0100

    feat: huddle updates + fixes

--- src/view/PermanentHires/index.jsx
+++ src/view/PermanentHires/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import {isHuddle} from '../../services/helper';
 
 import Hero from '../shared/SpecialismHero';
 import Leadership from '../shared/Specialists';
 import ProPoints from '../shared/ProPoints';
 import WorkForUs from '../shared/WorkForUs';
 
+const highlighted = isHuddle ? 'Permanent' : '';
+const title = isHuddle
+  ? ' hires to transform your team'
+  : 'Permanent hires that add high performance';
+
 const PermanentHires = () => (
   <div className='permanent-hires'>
     <Hero type='permanentHires'>
-      Permanent hires that add high performance
+      <a>{highlighted}</a>{title}
     </Hero>
     <ProPoints type='permanentHires' />
     <Leadership />
