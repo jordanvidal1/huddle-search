fae2a65e5e9dd6f58e851097cc56884ad9098597
jordanvidal1 <jv@coinburp.com>
2023-10-18 11:27:33 +0100

    feat: leaders

--- src/view/fonts.js
+++ src/view/fonts.js
@@ -21,8 +21,12 @@ import '@fontsource/playfair-display/900-italic.css'; // Specify weight and styl
 import '@fontsource/plus-jakarta-sans'; // Defaults to weight 400
 import '@fontsource/plus-jakarta-sans/400.css'; // Specify weight
 import '@fontsource/plus-jakarta-sans/400-italic.css'; // Specify weight and style
+import '@fontsource/plus-jakarta-sans/500.css'; // Specify weight
+import '@fontsource/plus-jakarta-sans/500-italic.css'; // Specify weight and style
 import '@fontsource/plus-jakarta-sans/600.css'; // Specify weight
 import '@fontsource/plus-jakarta-sans/600-italic.css'; // Specify weight and style
+import '@fontsource/plus-jakarta-sans/700.css'; // Specify weight
+import '@fontsource/plus-jakarta-sans/700-italic.css'; // Specify weight and style
 import '@fontsource/plus-jakarta-sans/800.css'; // Specify weight
 import '@fontsource/plus-jakarta-sans/800-italic.css'; // Specify weight and style
 
