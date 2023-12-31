36042a9dfc9c41986851813fb7af555b3c2b0842
jordanvidal1 <jv@coinburp.com>
2023-12-31 15:37:43 +0000

    fix: unitas pro points

--- src/view/shared/LeadershipHero.jsx
+++ src/view/shared/LeadershipHero.jsx
@@ -18,39 +18,39 @@ import {ReactComponent as Equals} from '../../static/icons/equal.svg';
 // import UnitasSignature2 from '../../static/unitas/signature-2.svg';
 // import UnitasSignature3 from '../../static/unitas/signature-3.svg';
 
-const leadershipComponent = t => (
-  <div className='info-container'>
-    <div className='info-box'>
-      <div className='info-title'>
-        <h5>
-          {t(`${NAMESPACE}:leadershipHero:leadershipTeam:info:title`)}
-        </h5>
-      </div>
-      <div className='info-details'>
-        <div className='info-detail'>
-          <h2>50</h2>
-          <span>Years experienced</span>
-        </div>
-        <div className='info-detail'>
-          <h2>10K</h2>
-          <span>Clients served</span>
-        </div>
-        <div className='info-detail'>
-          <h2>50K</h2>
-          <span>Candidates placed</span>
-        </div>
-        <div className='info-detail'>
-          <h2>100</h2>
-          <span>Sectors operated</span>
-        </div>
-        <div className='info-detail'>
-          <h2>250</h2>
-          <span>Specialisms mastered</span>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+// const leadershipComponent = t => (
+//   <div className='info-container'>
+//     <div className='info-box'>
+//       <div className='info-title'>
+//         <h5>
+//           {t(`${NAMESPACE}:leadershipHero:leadershipTeam:info:title`)}
+//         </h5>
+//       </div>
+//       <div className='info-details'>
+//         <div className='info-detail'>
+//           <h2>50</h2>
+//           <span>Years experienced</span>
+//         </div>
+//         <div className='info-detail'>
+//           <h2>10K</h2>
+//           <span>Clients served</span>
+//         </div>
+//         <div className='info-detail'>
+//           <h2>50K</h2>
+//           <span>Candidates placed</span>
+//         </div>
+//         <div className='info-detail'>
+//           <h2>100</h2>
+//           <span>Sectors operated</span>
+//         </div>
+//         <div className='info-detail'>
+//           <h2>250</h2>
+//           <span>Specialisms mastered</span>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 // const defaultSignature = {
 //   name: 'Name',
