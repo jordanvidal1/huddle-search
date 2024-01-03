d9093b7b083b878c9c787b1fe818604f575430f7
jordanvidal1 <jv@coinburp.com>
2024-01-03 16:58:19 +0000

    fix: lint

--- src/view/shared/Specialists.jsx
+++ src/view/shared/Specialists.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useTranslation} from 'react-i18next';
 import {
   LEADERS,
-  NAMESPACE,
+  NAMESPACE
 } from '../../data/constants';
 
 import {ReactComponent as Phone} from '../../static/icons/phone.svg';
